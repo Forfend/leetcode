e61ac198ae5fe8dc79242327dff9bd538744bcd6
Oleh Onufryk <oleh.onufryk@digitalturbine.com>
2021-11-22 23:54:09 +0200

    improved solution

--- src/main/java/com/leetcode/problems/easy/romanToInteger/RomanToInteger.java
+++ src/main/java/com/leetcode/problems/easy/romanToInteger/RomanToInteger.java
@@ -1,46 +1,43 @@
 package com.leetcode.problems.easy.romanToInteger;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class RomanToInteger {
-
-    private static final Map<Character, Integer> ROMAN_NUMBERS = new HashMap<>();
-
-    static {
-        ROMAN_NUMBERS.put('I', 1);
-        ROMAN_NUMBERS.put('V', 5);
-        ROMAN_NUMBERS.put('X', 10);
-        ROMAN_NUMBERS.put('L', 50);
-        ROMAN_NUMBERS.put('C', 100);
-        ROMAN_NUMBERS.put('D', 500);
-        ROMAN_NUMBERS.put('M', 1000);
-    }
 
     public int romanToInteger(String s) {
         int result = 0;
-        int coefficient;
-        char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            if (i > 0) {
-                if (isPreviousLess(chars[i], chars[i - 1])) {
-                    result -= ROMAN_NUMBERS.get(chars[i - 1]);
-                    coefficient = ROMAN_NUMBERS.get(chars[i]) - ROMAN_NUMBERS.get(chars[i - 1]);
-                } else {
-                    coefficient = ROMAN_NUMBERS.get(chars[i]);
-                }
-                result += coefficient;
+        int previous = Integer.MAX_VALUE;
+        for (int i = 0; i < s.length(); i++) {
+            int currentValue = convertCharToInt(s.charAt(i));
 
+            if (currentValue > previous) {
+                result += currentValue - previous - previous;
             } else {
-                result += ROMAN_NUMBERS.get(chars[i]);
+                result += currentValue;
             }
+            previous = currentValue;
         }
 
         return result;
     }
 
-    private boolean isPreviousLess(char current, char previous) {
-        return ROMAN_NUMBERS.get(current) > ROMAN_NUMBERS.get(previous);
+    private int convertCharToInt(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
     }
 
 }
