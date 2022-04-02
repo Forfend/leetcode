08fc6b9da788a0684280b90a7d0f8a91e3679194
Oleh Onufryk <oonufryk@atlassian.com>
2022-04-02 21:09:24 +0300

    [LeetCode 617]: Merge Two Binary Trees

---
+++ src/main/java/com/leetcode/problems/easy/mergeTwoBinaryTrees/MergeTwoBinaryTrees.java
@@ -0,0 +1,39 @@
+package com.leetcode.problems.easy.mergeTwoBinaryTrees;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+
+        if (root2 == null) {
+            return root1;
+        }
+
+        root1.val += root2.val;
+
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+
+        return root1;
+    }
+}
