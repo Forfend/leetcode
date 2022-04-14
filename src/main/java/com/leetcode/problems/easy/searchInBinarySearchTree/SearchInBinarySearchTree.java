fc889bad6ba6ba0d1f08b0682c1d29201621155c
Oleh Onufryk <oonufryk@atlassian.com>
2022-04-14 18:06:16 +0300

    [LeetCode 700]: Search in a Binary Search Tree

---
+++ src/main/java/com/leetcode/problems/easy/searchInBinarySearchTree/SearchInBinarySearchTree.java
@@ -0,0 +1,39 @@
+package com.leetcode.problems.easy.searchInBinarySearchTree;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SearchInBinarySearchTree {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root != null) {
+            if (root.val == val) {
+                return root;
+            } else if (root.val < val) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+
+        return null;
+    }
+
+}
