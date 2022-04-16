c11404f4fa1bf5e37f3d7f30c4010ce45e72ec65
Oleh Onufryk <oonufryk@atlassian.com>
2022-04-16 19:59:01 +0300

    [LeetCode 538]: Convert BST to Greater Tree

---
+++ src/main/java/com/leetcode/problems/medium/convertBSTToGreaterTree/ConvertBSTToGreaterTree.java
@@ -0,0 +1,35 @@
+package com.leetcode.problems.medium.convertBSTToGreaterTree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class ConvertBSTToGreaterTree {
+    private int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+
+        return root;
+    }
+}
