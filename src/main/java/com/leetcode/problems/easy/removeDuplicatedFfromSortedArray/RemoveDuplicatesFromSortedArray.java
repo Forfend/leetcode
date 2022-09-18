a37499fd3f5baf43c8237591e4f586c5c8688de6
Oleh Onufryk <olehonufryk@gmail.com>
2022-09-18 20:28:23 +0300

    [LeetCode 26]: Remove Duplicates from Sorted Array

---
+++ src/main/java/com/leetcode/problems/easy/removeDuplicatedFfromSortedArray/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,20 @@
+package com.leetcode.problems.easy.removeDuplicatedFfromSortedArray;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int k = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
