ce2f6f3951cee2fc354be32586c07bebc093c348
Oleh Onufryk <olehonufryk@gmail.com>
2024-03-13 15:04:40 +0200

    [LeetCode 206]: Reverse Linked List iteratively

--- src/main/java/com/leetcode/problems/easy/reverseLinkedList/ReverseLinkedList.java
+++ src/main/java/com/leetcode/problems/easy/reverseLinkedList/ReverseLinkedList.java
@@ -4,12 +4,25 @@ package com.leetcode.problems.easy.reverseLinkedList;
 import com.leetcode.problems.common.ListNode;
 
 public class ReverseLinkedList {
+
     public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public ListNode reverseListRecursion(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
 
-        ListNode node = reverseList(head.next);
+        ListNode node = reverseListRecursion(head.next);
         head.next.next = head;
 
         head.next = null;
