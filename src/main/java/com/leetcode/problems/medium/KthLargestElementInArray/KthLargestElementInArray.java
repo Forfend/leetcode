76d906283b6d54937df5a2483fd8186f156f0a96
Oleh Onufryk <olehonufryk@gmail.com>
2024-03-21 15:11:07 +0200

    [LeetCode 215]: added quickSelect implementation

--- src/main/java/com/leetcode/problems/medium/KthLargestElementInArray/KthLargestElementInArray.java
+++ src/main/java/com/leetcode/problems/medium/KthLargestElementInArray/KthLargestElementInArray.java
@@ -16,4 +16,49 @@ public class KthLargestElementInArray {
 
         return heap.peek();
     }
+
+    /**
+     * This method implements the quick select algorithm to find the kth largest element in an array.
+     *
+     * @param nums array of integers
+     * @param k    kth largest element
+     * @return kth largest element
+     */
+    public int findKthLargestQuickSelect(int[] nums, int k) {
+        k = nums.length - k;
+        return quickSelect(nums, 0, nums.length - 1, k);
+    }
+
+    private int quickSelect(int[] nums, int l, int r, int k) {
+        int pivot = nums[r];
+        int p = l;
+
+        for (int i = l; i < r; i++) {
+            if (nums[i] < pivot) {
+                int temp = nums[i];
+                nums[i] = nums[p];
+                nums[p] = temp;
+                p++;
+            }
+        }
+
+        int temp = nums[p];
+        nums[p] = nums[r];
+        nums[r] = temp;
+
+        if (k > p) {
+            return quickSelect(nums, p + 1, r, k);
+        } else if (k < p) {
+            return quickSelect(nums, l, p - 1, k);
+        } else {
+            return nums[p];
+        }
+    }
+
+    public static void main(String[] args) {
+        KthLargestElementInArray kthLargestElementInArray = new KthLargestElementInArray();
+        int[] nums = {3, 7, 1, 5, 6, 4};
+        int k = 2;
+        System.out.println(kthLargestElementInArray.findKthLargestQuickSelect(nums, k));
+    }
 }
